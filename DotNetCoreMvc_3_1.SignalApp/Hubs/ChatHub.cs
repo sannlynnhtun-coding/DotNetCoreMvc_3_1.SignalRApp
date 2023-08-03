4dfeab8a4ecdedcb6c015a7a9526460b60837f76
Sann Lynn Htun <sannlynnhtun.error@gmail.com>
2023-08-03 21:28:07 +0630

    Add project files.

---
+++ DotNetCoreMvc_3_1.SignalApp/Hubs/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetCoreMvc_3_1.SignalApp.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task ServerReceiveMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ClientReceiveMessage", user, message);
+        }
+    }
+}
---
+++ DotNetCoreMvc_3_1.SignalApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DotNetCoreMvc_3_1.SignalApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
